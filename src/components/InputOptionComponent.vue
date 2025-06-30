976e35bc876473d5405004c95dd44d20f08b639f
David Gilarranz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-30 18:27:04 +0200

    InputOptionComponent -> implemented basic functionality

---
+++ src/components/InputOptionComponent.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import { ref } from 'vue';
+
+  // Component props and emitted events
+  const props = defineProps({
+    label: String,
+    description: String,
+    id: String,
+    placeholder: String,
+    argumentName: String
+  });
+  const emit = defineEmits(['selected', 'deselected']);
+
+  // When clicked emit the appropriate event
+  function onInput(e) {
+    // Check if the input is empty to discern the event
+    if (e.target.value !== '') {
+      emit('selected', {
+        key: props.id,
+        arguments: [
+          {
+            name: props.argumentName,
+            value: e.target.value
+          }
+        ]
+      });
+    } else {
+      emit('deselected', { key: props.id });
+    }
+  };
+</script>
+
+<template>
+  <div :title="props.description">
+    <label :for="props.id">{{props.label}}</label>
+    <input :id="props.id" type="text" @input=onInput placeholder="props.placeholder" />
+  </div>
+</template>
---
+++ src/components/__tests__/InputOptionComponent.spec.js
@@ -0,0 +1,92 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import InputOptionComponent from '../InputOptionComponent.vue'
+
+describe('InputOptionComponent', () => {
+  // Options for the component
+  const props = {
+    label: 'Test',
+    id: 'test',
+    description: 'Test description',
+    placeholder: 'test placeholder',
+    argumentName: 'Argument Name'
+  };
+
+  it('Displays the label', () => {
+    // Render the Component
+    const wrapper = mount(InputOptionComponent, { props });
+
+    // Expect it to contain the title
+    expect(wrapper.text()).toContain(props.label);
+  });
+
+  it('Displays description when hovered', () => {
+    // Render the Component
+    const wrapper = mount(InputOptionComponent, { props });
+
+    // Expect the button to have title attribute set
+    expect(wrapper.find('div').attributes('title')).toEqual(props.description);
+  });
+
+  it('Emits a "selected" envent when input changes', () => {
+    // Render the Component
+    const wrapper = mount(InputOptionComponent, { props });
+
+    // Find and click on the button
+    wrapper.find('input').setValue('Test value');
+
+    // Expect the event to be emitted
+    expect(wrapper.emitted()).toHaveProperty('selected');
+  });
+
+  it('Passes its key and value as an argument when emitting the "selected" envent', () => {
+    // Render the Component
+    const wrapper = mount(InputOptionComponent, { props });
+
+    // Input a test value into the component
+    const testInput = 'Test';
+    wrapper.find('input').setValue(testInput);
+
+    // Expect the event to be emitted with its key
+    expect(wrapper.emitted('selected')[0]).toEqual([
+      {
+        key: props.id,
+        arguments: [
+          {
+            name: props.argumentName,
+            value: testInput
+          }
+        ]
+      }
+    ]);
+  });
+
+  it('Emits a "deselected" envent when the input is cleared', () => {
+    // Render the Component
+    const wrapper = mount(InputOptionComponent, { props });
+
+    // Input a test value into the component and clear it
+    wrapper.find('input').setValue('test input');
+    wrapper.find('input').setValue('');
+
+    // Expect the event to be emitted
+    expect(wrapper.emitted()).toHaveProperty('deselected')
+  });
+
+  it('Passes its id as an argument when emitting the "deselected" envent', () => {
+    // Render the Component
+    const wrapper = mount(InputOptionComponent, { props });
+
+    // Input a test value into the component and clear it
+    wrapper.find('input').setValue('test input');
+    wrapper.find('input').setValue('');
+
+    // Expect the event to be emitted with its key
+    expect(wrapper.emitted('deselected')[0]).toEqual([
+      {
+        key: props.id
+      }
+    ])
+  });
+});
