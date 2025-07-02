dbb913c835ed1eb916432bdb3f846b43bb499509
David Gilarranz Ortega <david.gilarranz016@comunidadunir.net>
2025-07-02 14:32:16 +0200

    HomeScreen -> navigates to shell selection screen

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -1,20 +1,30 @@
 <script setup>
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
+
+  const onNavigate = () => {
+    router.push('/shell');
+  };
 </script>
 
 <template>
-  <h1>Welcome to the Web Shell Generator!</h1>
-  <p>
-    This tool will guide you through the process of generating 
-    a secure and modular web shell to use in CTFs, bug bounty programs or client 
-    engagements.
-  </p>
-  <p>
-    You will be able to select the features you want the shell to have, including
-    some optional security features in addition to the default ones.
-    The resulting web shell will be comprised of two elements:
-  </p>
-  <ul>
-    <li>shell: this component will need to be uploaded to the target.</li>
-    <li>client: this component will allow secure communication with the shell.</li>
-  </ul>
+  <div>
+    <h1>Welcome to the Web Shell Generator!</h1>
+    <p>
+      This tool will guide you through the process of generating 
+      a secure and modular web shell to use in CTFs, bug bounty programs or client 
+      engagements.
+    </p>
+    <p>
+      You will be able to select the features you want the shell to have, including
+      some optional security features in addition to the default ones.
+      The resulting web shell will be comprised of two elements:
+    </p>
+    <ul>
+      <li>shell: this component will need to be uploaded to the target.</li>
+      <li>client: this component will allow secure communication with the shell.</li>
+    </ul>
+    <button @click="onNavigate">Start</button>
+  </div>
 </template>
--- src/components/__tests__/HomeScreen.spec.js
+++ src/components/__tests__/HomeScreen.spec.js
@@ -1,7 +1,14 @@
-import { describe, it, expect } from 'vitest'
-
+import { describe, it, expect, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
+import { useRouter } from 'vue-router';
 import HomeScreen from '../HomeScreen.vue'
+
+// Mock the Router
+vi.mock('vue-router', () => ({
+  useRouter: vi.fn(() => ({
+    push: () => {}
+  }))
+}));
 
 describe('HomeScreen', () => {
   it('Includes the title', () => {
@@ -11,5 +18,19 @@ describe('HomeScreen', () => {
     // Expect it to contain the title
     const title = 'Welcome to the Web Shell Generator!';
     expect(wrapper.text()).toContain(title);
+  });
+
+  it('Navigates to the Shell Technology selection page', () => {
+    // Mock the router to return a stub
+    const push = vi.fn();
+    useRouter.mockImplementationOnce(() => ({ push }));
+
+    // Render the HomeScreen
+    const wrapper = mount(HomeScreen);
+
+    // Find and click the navigation button
+    wrapper.find('button').trigger('click');
+
+    expect(push).toHaveBeenCalledExactlyOnceWith('/shell');
   });
 });
